06296f091d868819a7fa0e4e0f2c14d085af1f14
SaraAlsubaieNya <saralsubaieph@gmail.com>
2025-05-28 22:11:01 +0300

    Update nlpai_tweets.py

--- nlpai_tweets.py
+++ nlpai_tweets.py
@@ -14,11 +14,8 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 
-!pip install vaderSentiment
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('vader_lexicon')
+#!pip install vaderSentiment
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
